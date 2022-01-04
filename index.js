f5f5dd41a9f513f5f6751465080f9d8e207e1bea
JANO <77622056+Devjano@users.noreply.github.com>
2022-01-03 18:15:04 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,199 @@
+
+<html>
+
+    <head>
+  
+  <!-- file bustrap-->
+  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  
+  <!-- file css  -->
+    
+  <link rel="stylesheet" href="style.css">
+  <link href="https://pro.fontawesome.com/releases/v5.11.2/css/all.css" rel="stylesheet">
+  
+      
+  
+  
+   
+  
+    </head>
+    
+  <body>
+      
+    <div>
+    
+  <!-- Navbar -->
+  
+  
+  
+   <nav id="home" style="background: #26272b; color: tomato;;" class="navbar navbar-expand-lg navbar-dark">
+      <a class="navbar-brand" href="#"><img class="allegro" src="https://images-ext-2.discordapp.net/external/yo3jB2aJXXy_W5HGm1PiCVW6rB21AxbkX9zXVRk9yfE/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/780578520870158337/8e44e7e17f71a26e7668fea9c3b67d59.png?width=473&height=473"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarColor01">
+      <li class="nav-item activee">
+          <a class="nav-link" href="social media.html"> <i class="fad fa-home"></i> <b class="btn btn-danger">social media</b> </a>
+        </li>
+  
+  
+  
+  <!-- support server-->
+                  <li class="nav-item activee">
+            <a class="nav-link" href="https://discord.gg/dTNKbPXw9Y"> <i class="fab fa-discord"></i> <b class="btn btn-danger">Support Server</b> </a>
+          </li>
+  
+  <!-- link bot  -->
+                  <li class="nav-item activee">
+            <a class="nav-link" href="https://discord.com/api/oauth2/authorize?client_id=780578520870158337&permissions=8&scope=bot"><i class="fad fa-user-plus"></i> <b class="btn btn-danger">Invite To Server</b> </a>
+          </li>
+  
+  <!-- vote bot -->
+                          <li class="nav-item activee">
+            <a  class="nav-link" href="https://top.gg/bot/780578520870158337/vote"><i class="fad fa-plus"> </i> <b class="btn btn-danger">vote</b> </a>
+          </li>
+          
+        </ul>
+      </div>
+    </nav>
+    
+  
+  
+  <div class="Header">
+  
+    <div class="Header-background"></div>
+    <div class="Header-content">
+      <div class="Header-hero">
+  
+          <button onclick="location.href = 'https://discord.com/api/oauth2/authorize?client_id=780578520870158337&permissions=8&scope=bot';" class="Button"> <strong> Invite To Server</strong> </button>
+  <br>
+  <br>
+        <pre style="color: white;"> <strong> Hyper music commands with Features, listening to the music with full quality.
+     Supported by many music streaming platforms.
+  
+     •YouTube •SoundCloud </strong> </pre>
+  
+  
+      </div>
+  
+      <div class="mobile">
+        <div data-aos="zoom-out-left">
+  
+      <div class="Header-visuals">
+  <div style="margin-left: 15%; margin-bottom: 100%;" class="player">
+      <nav>
+          <div class="left">
+              <i class="fad fa-ellipsis-v"></i>
+              <h6 style="color:tomato;"> <strong>Hyper Music</strong></h6>
+          </div>
+          <div class="right">
+              <i class="fad fa-search"></i>
+              <i class="fad fa-queue"></i>
+          </div>
+  
+      </nav>
+  <!-- #bashe title img -->
+      <div class="player-ui">
+          <div class="title">
+              <h3> <b> Song </b> </h3>
+          </div>
+  
+          <!-- #bashe rasmaka lalay title -->
+          <div class="small">
+              <i class="fad fa-undo-alt"></i>
+              <p style="color:rgb(247, 242, 242);"> <strong>Next</strong> </p>
+              <i class="fad fa-volume"></i>
+          </div>
+  <!-- #Stop -->
+          <div class="progress">
+              <div class="played">
+                  <div class="circle"></div>
+              </div>
+          </div>
+  
+          <div class="controls">
+                  <i class="fad fa-backward"></i>
+                  <i class="fad fa-play"></i>
+                  <i class="fad fa-forward"></i>
+          </div>
+  
+      </div>
+      <br><br><br>
+      <div class="music">
+          <div class="song-1">
+              <div class="info">
+                  <div class="titles">
+                      <h5 style="color: white;"> <b>Volume</b>   </h5>
+                      <pre style="color:white">The rate has been raised 10%</pre>
+                  </div>
+              </div>
+              <div class="state playing">
+                  <i class="fad fa-volume-up"></i>
+              </div>
+          </div>
+          <div class="song-2">
+              <div class="info">
+                  <div class=""></div>
+                  <div class="titles">
+                      <h5 style="color: white;"><strong>Stopped</strong></h5>
+                      <pre style="color: white;">Music queue ended</pre>
+                  </div>
+              </div>
+              <div class="state">
+                  <i class="fad fa-play"></i>
+              </div>
+  
+  
+          </div>
+  
+          <div class="song-3">
+              <div class="info">
+                  <div class="titles">
+                  
+                      <pre style="color: white;">graduated from Voissn</pre>
+                  </div>
+  
+              </div>
+  
+              <div class="state">
+                  <i class="fad fa-play"></i>
+              </div>
+          </div>
+          <div class="song-4">
+              <div class="info">
+                  <div class=""></div>
+                  <div class="titles">
+                      <h5>Purple rain</h5>
+                      <p>Prince & The Revolution</p>
+                  </div>
+              </div>
+              <div class="state">
+                  <i class="fad fa-play"></i>
+              </div>
+          </div>
+      </div>
+  </div>    </div>
+    </div>
+  </div>
+    </div>  
+  </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#26272b" fill-opacity="1" d="M0,192L48,192C96,192,192,192,288,165.3C384,139,480,85,576,85.3C672,85,768,139,864,170.7C960,203,1056,213,1152,208C1248,203,1344,181,1392,170.7L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+  
+  </body>
+  </html>
+  
+  <!-- #js -->
+  <div class="container">
+  
+      <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  
+  </div>
+  
+  <!-- #about -->
+  
